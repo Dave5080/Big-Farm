3415861f609e5a25298df8d0437cf940e7051b19
Dave5080 <damicosalvato@gmail.com>
2022-05-22 14:34:05 +0200

    Pseudo finished collector, fixed socket offset, tests ecc

--- collector.py
+++ collector.py
@@ -2,30 +2,75 @@
 
 import socket
 import _thread
+import threading
+
+coll = dict()
+coll_sem = threading.Semaphore()
 
 HOST = "127.0.0.1"
 PORT = 8888
+sep = "\n"
 
-def on_new_client(clientsocket,addr):
-    sep = "\n"
+def fixstr(oldstr):
+    return oldstr.replace("\n","").replace("\x00","");
+
+def on_new_worker_client(clientsocket,addr):
     
-    filename = ''
-    sum = ''
-    pid = ''
-    tid = ''
-    while sep not in filename:
-        data = clientsocket.recv(2)
-        filename += data.decode("utf8")
-    while sep not in sum:
-        data = clientsocket.recv(2)
-        sum += data.decode("utf8")
-    while sep not in pid:
-        data = clientsocket.recv(2)
-        pid += data.decode("utf8")
-    while sep not in tid:
-        data = clientsocket.recv(2)
-        tid += data.decode("utf8")
-    print("{} {}/{} >> \t{}\t{}\n".format(addr,pid.replace("\n",""),tid.replace("\n",""),filename.replace("\n",""),sum.replace("\n","")))
+    while True:
+        filename = ''
+        sum = ''
+        pid = ''
+        tid = ''
+        while sep not in filename:
+            data = clientsocket.recv(1)
+            filename += data.decode("utf8")
+        if "\r" in filename:
+            break
+        while sep not in sum:
+            data = clientsocket.recv(1)
+            sum += data.decode("utf8")
+        while sep not in pid:
+            data = clientsocket.recv(1)
+            pid += data.decode("utf8")
+        while sep not in tid:
+            data = clientsocket.recv(1)
+            tid += data.decode("utf8")
+        coll_sem.acquire()
+        if "tests" not in fixstr(filename):
+            print("PROBLEMAAAAA")
+        coll[fixstr(filename)] = int(fixstr(sum))
+        print("{} {}/{} >> {} {}".format(addr,fixstr(pid),fixstr(tid),fixstr(filename),coll[fixstr(filename)]))
+        coll_sem.release()
+    print("Connecion from {} closed.".format(addr))
+
+def on_new_logging_client(clientsocket,addr):
+    mode = ''
+
+    while sep not in mode:
+        data = clientsocket.recv(1)
+        mode += data.decode("utf-8")
+    coll_sem.acquire()
+    mydic = sorted(coll.items(), key=lambda x: x[1])
+    if "all" in mode:
+        if len(coll) >= 1:
+            for (filename, sum) in mydic:
+                clientsocket.send("{}\n{}\n".format(filename,sum).encode())
+                print("Sending ({}:{} to {})".format(filename, sum, addr))
+        else:
+            clientsocket.send("Nessun file\n".encode())
+            print("Sending Nessun file")
+    else:
+        mysum = int(fixstr(mode))
+        count = 0
+        for (filename, sum) in mydic:
+            if sum == mysum:
+                count += 1
+                clientsocket.send("{}\n{}\n".format(filename,sum).encode())
+                print("Sending special ({}:{}) to {}".format(filename, sum,addr))
+        if count < 1:
+            clientsocket.send("{}\n{}\n".format(filename,sum).encode())
+            print("Sending ({}:{} to {})".format(filename, sum, addr))
+    coll_sem.release()
 
 s = socket.socket()
 s.bind((HOST, PORT))
@@ -33,11 +78,21 @@ s.bind((HOST, PORT))
 print("Server started!")
 print("Waiting for clients...")
 
-s.listen(5)
+s.listen(8)
 
 while True:
+    buf = ''
     c, addr = s.accept()
-    print('Got connection from {}'.format(addr))
-    _thread.start_new_thread(on_new_client,(c,addr))
+    while sep not in buf:
+        data = c.recv(1)
+        buf += data.decode("utf8")
+     
+    if "worker" in buf:
+        print('Got worker connection from {}'.format(addr))
+        _thread.start_new_thread(on_new_worker_client,(c,addr))
+    else: 
+        if "client" in buf:
+            print('Got client connection from {}'.format(addr))
+            _thread.start_new_thread(on_new_logging_client,(c,addr))
 
 s.close()
--- farm.c
+++ farm.c
@@ -82,17 +82,17 @@ int sock_init_connect(){
   struct sockaddr_in address;
 
   sock = xsocket(AF_INET, SOCK_STREAM, 0, QUI);
-
+  //setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,true,sizeof(int));
   address.sin_family = AF_INET;
 	inet_pton(AF_INET, "127.0.0.1", &(address.sin_addr));
 	address.sin_port = htons( 8888 );
 
   printf("[Farm] Connecting...\n");
-  client_fd = connect(sock, (struct sockaddr*)&address, sizeof(address))
-    ;
-  printf("[Farm] Connected\n");
-  //xbind(server_fd, (struct sockaddr *)&address, sizeof address, QUI);
+  client_fd = connect(sock, (struct sockaddr*)&address, sizeof(address));
+  send(sock, "worker\n", sizeof("worker"), 0);
+  printf("[Farm] Connected!\n");
 
+  //xbind(server_fd, (struct sockaddr *)&address, sizeof address, QUI);
   //xlisten(server_fd, 1, QUI);
 
   xsem_init(&sem_client, 0, 1, QUI);
@@ -104,19 +104,16 @@ int sock_send_couple(char* filename, long sum, int pid, int tid){
 
   xsem_wait(&sem_client, QUI);
 
-  char ssum[15] = "", spid[15] = "", stid[15] = "";
+  char ssum[16] = "", spid[16] = "", stid[16] = "";
+  char sfilename[256];
 
 
   sprintf(ssum, "%ld\n", sum);
   sprintf(spid, "%d\n", pid);
   sprintf(stid, "%d\n", tid);
+  sprintf(sfilename, "%s\n", filename);
 
-  printf("Sending sum: %ld -> %s", sum, ssum);
-  printf("Sending pid: %d -> %s", pid, spid);
-  printf("Sending tid: %d -> %s", tid, stid);
-
-  send(sock, filename, strlen(filename), 0);
-  send(sock, "\n", strlen("\n"), 0);
+  send(sock, sfilename, strlen(sfilename), 0);
   printf("Sent: %s \t\t\t", filename);
 
   send(sock, ssum, sizeof(ssum), 0);
@@ -124,7 +121,7 @@ int sock_send_couple(char* filename, long sum, int pid, int tid){
 
 
   send(sock, spid, sizeof(spid), 0);
-  printf(" %d/", pid);
+  printf(" %d\n", pid);
 
 
   send(sock, stid, sizeof(stid), 0);
@@ -191,6 +188,9 @@ int main(int argc, char *argv[]){
   for(int t = 0; t < nthread; t++)
     pthread_join(workers[t], NULL);
   // Free memory
+  send(sock, "\r\n", sizeof("\r\n"), 0);
+  close(client_fd);
+  close(sock);
   freeQueue(files);
   free(workers);
 
